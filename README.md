571c893c6cfcfea629a76250e40667e8fecc0c02
Tyler Pachal <tylerpachal@Tylers-MacBook-Pro-5.local>
2015-04-03 17:41:01 -0300

    Initial commit.

--- README.md
+++
@@ -1,2 +0,0 @@
-# npm-earley-cfg
-An implementation of the a Context Free Grammar (CFG) using the Earley algorithm in Node.JS
---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+	Grammar : require("./lib/grammar"),
+	Sentence: require("./lib/sentence"),
+	Parser : require("./lib/parser"),
+	ParseTrees : require("./lib/parse_trees")
+}
